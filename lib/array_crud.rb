c641f6eb02f87b4ce1889cb394e95dcd3ff0091f
biancacharlotin <biancacharlotin@gmail.com>
2018-11-07 19:48:13 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array.push ("array!")
+  create_an_array.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  create_an_array.pop ("arrays!")
+  create_an_array.pop [5]
 end
 
 def remove_element_from_start_of_array(array)
