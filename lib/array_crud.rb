65ff36cdb687b2f07aad319317b48e7999d9a824
biancacharlotin <biancacharlotin@gmail.com>
2018-11-07 19:35:57 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array.push ("vageta")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshif("luffy")
 end
 
 def remove_element_from_end_of_array(array)
